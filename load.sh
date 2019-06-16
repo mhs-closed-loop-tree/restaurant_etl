5e82be75c2ea7564ffe41ee30ec74a55febd0c5f
Mike Smith <mike.smith.mertex@gmail.com>
2019-06-16 15:30:51 +0100

    Example log file generated (with QUIET enabled)

--- load.sh
+++ load.sh
@@ -11,7 +11,7 @@ source venv/bin/activate
 pip install -r requirements.txt
 
 # Note: set to '-q' to hide the step output and interactive prompts
-QUIET=
+QUIET=-q
 
 LOAD_DATE=`date +%Y%m%d`
 IN_FILENAME=restaurant.in.$LOAD_DATE.csv
