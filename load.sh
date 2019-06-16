6cf51c5f4159457e2f98f5ca03abf20c9287d528
Mike Smith <mike.smith.mertex@gmail.com>
2019-06-16 13:50:58 +0100

    Staging health check

--- load.sh
+++ load.sh
@@ -22,40 +22,12 @@ TABLE_NAME=restaurant__staging
 DATABASE_NAME=tree
 DATABASE_USER_NAME=tree
 INSTANCE_NAME=tree
+STAGING_THRESHOLD=150000
 
 
 
 echo Fetching data file...
 curl https://data.cityofnewyork.us/api/views/43nn-pn8j/rows.csv?accessType=DOWNLOAD -o$IN_FILENAME
-# Handes the end to end load sequence, fetch, transform, validate, load, and SQL transform
-# Requirements:  psql 		(including ~/.pgpass)
-#                gcloud sdk
-
-source trap.sh
-
-python3 -m venv venv
-
-source venv/bin/activate
-
-pip install -r requirements.txt
-
-# Note: set to '-q' to hide the step output and interactive prompts
-QUIET=
-
-LOAD_DATE=`date +%Y%m%d`
-IN_FILENAME=restaurant.in.$LOAD_DATE.csv
-OUT_FILENAME=restaurant.out.$LOAD_DATE.csv
-
-BUCKET_NAME=tree-20190615
-TABLE_NAME=restaurant__staging
-DATABASE_NAME=tree
-DATABASE_USER_NAME=tree
-INSTANCE_NAME=tree
-
-
-
-echo Fetching data file...
-#curl https://data.cityofnewyork.us/api/views/43nn-pn8j/rows.csv?accessType=DOWNLOAD -o$IN_FILENAME
 
 echo Preparing and Validating the data...
 python src/pre_process.py $IN_FILENAME $OUT_FILENAME
@@ -71,5 +43,11 @@ psql -h $SQL_IP -U $DATABASE_USER_NAME -d $DATABASE_NAME < sql/truncate_staging.
 echo Starting SQL Import to staging table...
 gcloud $QUIET sql import csv $INSTANCE_NAME gs://$BUCKET_NAME/$OUT_FILENAME --database=$DATABASE_NAME --table=$TABLE_NAME
 
+echo Checking staging table...
+LOADED_COUNT=`psql -h $SQL_IP -U $DATABASE_USER_NAME -d $DATABASE_NAME < sql/check_staging.sql | sed '3!d'`
+if [ "$LOADED_COUNT" -lt "$STAGING_THRESHOLD" ]; then
+  echo Data in staging looks suspect. $LOADED_COUNT rows is below the threshold of $STAGING_THRESHOLD. Please investigate.
+  exit 3
+fi
 
-
+echo Proceed
---
+++ sql/check_staging.sql
@@ -0,0 +1,1 @@
+select count(camis) from restaurant__staging where valid=1 and grade is not null;
